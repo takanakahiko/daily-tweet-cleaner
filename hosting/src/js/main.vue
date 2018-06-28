01a6de1369cddc02c146c30cbd345c3f9d1d3210
takanakahiko <takanakahiko@gmail.com>
2018-06-29 03:21:20 +0900

    add welcome message

--- hosting/src/js/main.vue
+++ hosting/src/js/main.vue
@@ -10,6 +10,7 @@
       <el-col :span="12" :offset="6" :xs="{span:24, offset:0}">
         <el-card class="box-card">
           <div slot="header" class="clearfix">ようこそ</div>
+          <p>このサービスを使用するためにはTwitter連携が必要です．</p>
           <login></login>
         </el-card>
       </el-col>
